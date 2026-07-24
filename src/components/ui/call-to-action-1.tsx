7eb5e663166401101b9050a6ab90a040aa36ddde
Marcello-calyps <153430442+Martzcode@users.noreply.github.com>
2026-07-24 13:57:22 +0300

    Optimisation mobile
    - Ajout format image
    - Suppression import google font

--- src/components/ui/call-to-action-1.tsx
+++ src/components/ui/call-to-action-1.tsx
@@ -5,14 +5,6 @@ import type { Dictionary } from "@/i18n/dictionaries";
 export default function Example({ dict }: { dict: Dictionary["contact"]["cta"] }) {
   return (
     <>
-      <style>{`
-                @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
-                * {
-                    font-family: 'Poppins', sans-serif;
-                }
-            `}</style>
-
       <div className="max-w-5xl py-16 md:w-full mx-2 md:mx-auto flex flex-col items-center justify-center text-center bg-gradient-to-b from-[#5524B7] to-[#380B60] rounded-2xl p-10 text-white">
         <div className="flex flex-wrap items-center justify-center p-1 rounded-full bg-purple-600/10 backdrop-blur border border-purple-500/40 text-sm">
           <div className="flex items-center">
